1d81e9f488be6b5843712212517f3673d59d54b0
Ibrahim Akancay <iakancay@gmail.com>
2022-07-10 20:39:48 +0200

    ex1, 2 and 3 is done

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -17,29 +17,36 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
-function requestData(url) {
-  // TODO return a promise using `fetch()`
+async function requestData(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Ooops something went wrong!');
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
-  console.log(data);
+  const imgElement = document.createElement('img');
+  const body = document.querySelector('body');
+  imgElement.src = data.img;
+  imgElement.alt = data.alt;
+  body.appendChild(imgElement);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
-  console.log(error);
+  const errorElement = document.createElement('h1');
+  const body = document.querySelector('body');
+  errorElement.textContent = `${error}`;
+  body.appendChild(errorElement);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (err) {
+    renderError(err);
+  }
 }
 
 window.addEventListener('load', main);
--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,4 +1,7 @@
 'use strict';
+
+//const { indexOf } = require('lodash');
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
 
@@ -16,24 +19,65 @@ Complete the four functions provided in the starter `index.js` file:
 
 `main`: The `main` function orchestrates the other functions. The `main` 
   function should be executed when the window has finished loading.
-
+  
+  //https://pokeapi.co/api/v2/pokemon
 Use async/await and try/catch to handle promises.
 
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+const container = document.querySelector('.container');
+const button = document.createElement('button');
+button.type = 'button';
+button.textContent = 'Get Pokemon!';
+container.appendChild(button);
+const selectElement = document.createElement('select');
+container.appendChild(selectElement);
+const imgElement = document.createElement('div');
+container.appendChild(imgElement);
+const api = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const imgApi = `https://pokeapi.co/api/v2/pokemon-form/`;
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchAndPopulatePokemons(url) {
+  try {
+    const data = await fetchData(url);
+    selectElement.textContent = '';
+    data.results.forEach((pokemon) => {
+      const option = document.createElement('option');
+      option.value = data.results.indexOf(pokemon) + 1;
+      option.textContent = pokemon.name;
+      selectElement.appendChild(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(url) {
+  try {
+    const select = document.querySelector('select');
+    const pokemonIndex = select.options[select.selectedIndex].value;
+    const data = await fetchData(`${url}${pokemonIndex}/`);
+    const imgUrl = data.sprites.front_default;
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    imgElement.textContent = ' ';
+    imgElement.appendChild(img);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function main() {
-  // TODO complete this function
+  button.addEventListener('click', () => fetchAndPopulatePokemons(api));
+  selectElement.addEventListener('change', () => fetchImage(imgApi));
 }
+window.addEventListener('load', main);
